da29b5648b88c3c0a75d655457c92c2e5918354e
adringutierrez <aldrin.gutierrez@hotmail.com>
2016-04-06 13:33:39 -0500

    Procfile

--- target/generated-sources/annotations/com/aeg/climav1/Clientes_.java
+++ target/generated-sources/annotations/com/aeg/climav1/Clientes_.java
@@ -5,18 +5,18 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-05T23:47:31")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-06T13:24:27")
 @StaticMetamodel(Clientes.class)
 public class Clientes_ { 
 
-    public static volatile SingularAttribute<Clientes, String> nombre;
+    public static volatile SingularAttribute<Clientes, Integer> idcli;
+    public static volatile SingularAttribute<Clientes, Integer> ciudad;
     public static volatile SingularAttribute<Clientes, String> apellido;
     public static volatile SingularAttribute<Clientes, String> estadocivil;
-    public static volatile SingularAttribute<Clientes, Integer> ciudad;
-    public static volatile SingularAttribute<Clientes, Integer> departamento;
-    public static volatile SingularAttribute<Clientes, Integer> idcli;
-    public static volatile SingularAttribute<Clientes, Integer> sueldo;
     public static volatile SingularAttribute<Clientes, String> correo;
+    public static volatile SingularAttribute<Clientes, Integer> sueldo;
+    public static volatile SingularAttribute<Clientes, Integer> departamento;
+    public static volatile SingularAttribute<Clientes, String> nombre;
     public static volatile SingularAttribute<Clientes, Date> fecnacimiento;
 
 }
